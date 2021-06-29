f9ecf4fa686315e7e58df1ba33886f44528b8592
xVilandx <malabarvovk18@gmail.com>
2021-06-29 16:20:07 +0300

    HW5 v2 done

--- HW5.py
+++ HW5.py
@@ -28,7 +28,7 @@ for index, symbol in enumerate(my_list_2):
 
 # 4
 my_list = [1, 2, 3, 4, 5, 6]
-new_list = my_list[1:]
+new_list = my_list.copy()[1:]
 new_list.append(my_list[0])
 
 print(new_list)
