9778b3b6581929cf647cfbd6f8441d783afd66e8
xVilandx <malabarvovk18@gmail.com>
2021-08-01 18:09:01 +0300

    HW 10 done

--- HW10/HW10.py
+++ HW10/HW10.py
@@ -1,45 +1,38 @@
 #1 task
-def create_list_domains(file_name):
-    with open(file_name, 'r') as txt_file:
-        return txt_file.read().replace('.', '').split('\n')
-
-
-#2 task
-def create_list_surname(file_name):
-    with open(file_name, 'r') as txt_file:
-        return txt_file.read().split('\t')[1::3]
+# def create_list_domains(file_name):
+#     with open(file_name, 'r') as txt_file:
+#         return txt_file.read().replace('.', '').split('\n')
+#
+#
+# #2 task
+# def create_list_surname(file_name):
+#     with open(file_name, 'r') as txt_file:
+#         return txt_file.read().split('\t')[1::3]
 
 
 #3 task
 def create_list_date(file_name):
     with open(file_name, 'r') as txt_file:
-        list_date = [my_str[0:my_str.find('-')-1] for my_str in txt_file.read().split('\n') if my_str.count('-') > 0]
+        list_date = [date for date in txt_file.read().split('\n') if date.count('-') > 0]
+        list_date = [date.split(' - ')[0] for date in list_date if len(date[0:date.find('-')].split()) == 3]
         return list_date
 
 
-def modified_date(file_name):
-    modified_list_date = []
+def modified_date(date_line):
     month_dict = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06',
                   'July': '07', 'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12'}
-    for date in create_list_date(file_name):
-        my_string = ''
-        for value in date.split():
-            if value[0].isdigit() and not value[2].isdigit():
-                if len(value) == 3:
-                    my_string += '0' + value[0] + '/'
-                else:
-                    my_string += value[:2] + '/'
-            if month_dict.get(value):
-                my_string += month_dict[value]
-            if value.isdigit():
-                my_string += '/' + value
-        modified_list_date.append(my_string)
-    return modified_list_date
+    day, months, year = date_line.split()
+    day = day.rstrip('ndrsth')
+    day = day if len(day) == 2 else f'0{day}'
+    months = month_dict[months]
+    new_date = f'{day}/{months}/{year}'
+    return new_date
 
 
 def create_dict(file_name):
-    full_list_date = []
+    full_list = []
     for value in range(len(create_list_date(file_name))):
-        my_dict = dict(date_original=create_list_date(file_name)[value], date_modified=modified_date(file_name)[value])
-        full_list_date.append(my_dict)
-    return full_list_date
+        date = create_list_date(file_name)[value]
+        date_dict = dict(date_original=date, date_modified=modified_date(date))
+        full_list.append(date_dict)
+    return full_list
